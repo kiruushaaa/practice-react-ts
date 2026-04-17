fc018aae35b8ab5e373c743a1934671a820f86e9
kiruushaaa <kiruxa4866613@gmail.com>
2026-04-17 13:59:35 +0300

    feat: replace prettier with oxfmt

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from 'eslint-config-prettier';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
@@ -21,5 +20,4 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-  eslintConfigPrettier,
 ]);
